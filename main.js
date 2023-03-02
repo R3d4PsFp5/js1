4d44037acede64855afd3600d3c96282c1cae378
Sergey <sergey3>
2023-03-02 21:23:13 +0500

    v1

---
+++ main.js
@@ -0,0 +1,17 @@
+var Name = prompt('Введите свое имя');
+console.log("Мое имя " + Name);
+var age = prompt('Введите свой возраст');
+console.log('Мне ' + age);
+var primer1 = prompt('Сколько будеь 6 + 9 = ?');
+console.log('Пример 1: 6 + 9 = 15 Ваш ответ: ' + primer1);
+
+var primer2 = prompt('Сколько будеь 16 - 9 = ?');
+console.log('Пример 2: 16 - 9 = 7 Ваш ответ: ' + primer2);
+
+var primer3 = prompt('Сколько будеь 6 * 2 = ?');
+console.log('Пример 3: 6 * 2 = 12 Ваш ответ: ' + primer3);
+
+var primer4 = prompt('Сколько будеь 6 / 3 = ?');
+console.log('Пример 4: 6 / 3 = 2 Ваш ответ: ' + primer4);
+
+alert('Посмотрите в кансоль!');
